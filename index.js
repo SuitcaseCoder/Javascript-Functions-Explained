c5264ffbd58fff11a4ee46e624dbd4525ed1e4b3
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-03-22 14:31:27 -0500

    javascript fnctions explained - YOUTUBE

---
+++ index.js
@@ -0,0 +1,39 @@
+// JAVASCRIPT FUNCTIONS
+
+// write a function
+// function nameYourFunction(parameters,go,here){
+//     // code goes here
+//     console.log('hello world');
+//     // make sure to return something
+//     return `Congrats! ${parameters} ${go} ${here}`;
+// };
+
+// // execute your function, pass in any arguments you need for your function to run\
+// nameYourFunction('you', 'did', 'it');
+
+
+// add two numbers
+// function addTwoNumbers(num1, num2){
+//     let sum = num1 + num2;
+//     return sum; //
+// }
+
+// console.log(addTwoNumbers(4,6)); //10
+// console.log(addTwoNumbers(3,2)); // 5
+
+
+
+// function nameYourFunction(params){
+//     //code goes here
+//     return `Thanks for the tutorial, ${params}!`
+// }
+
+// nameYourFunction('Suitcase Coder')
+
+// // PRACTICE PROBLEMS:
+
+// // 1. WRITE A FUNCTION THAT ACCEPTS A STRING AND RETURNS A GREETING MESSAGE (EX: "WELCOME TO THE WORLD OF CODING, LAURA");
+
+// // 2. WRITE A FUNCTION THAT ACCEPTS AN ARRAY OF NUMBERS AND RETURNS THE SUM OF ALL THE NUMBERS IN THE ARRAY (HINT: YOU'LL HAVE TO LOOP THROUGH THE ARRAY)
+
+// // 3. WRITE A FUNCTION THAT ACCEPTS TWO NAMES AND RETURNS WHETHER OR NOT THE TWO NAMES HAVE THE SAME NUMBER OF LETTERS (HINT: SHOULD RETURN A BOOLEAN)
